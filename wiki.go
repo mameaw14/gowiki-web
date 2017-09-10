7fe78798ee8826413a968aa50d01c1cb486c5ee6
mameaw14 <pinyapornster@gmail.com>
2017-09-10 23:43:15 +0700

    html/template

--- wiki.go
+++ wiki.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
     "io/ioutil"
     "net/http"
+    "html/template"
 )
 
 type Page struct {
@@ -25,14 +25,30 @@ func loadPage(title string) (*Page, error) {
     return &Page{Title: title, Body: body}, nil
 }
 
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+    t, _ := template.ParseFiles(tmpl + ".html")
+    t.Execute(w, p)
+}
+
 func viewHandler(w http.ResponseWriter, r *http.Request) {
     title := r.URL.Path[len("/view/"):]
     p, _ := loadPage(title)
-    fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+    renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/edit/"):]
+    p, err := loadPage(title)
+    if err != nil {
+        p = &Page{Title: title}
+    }
+    renderTemplate(w, "edit", p)
 }
 
 func main() {
     http.HandleFunc("/view/", viewHandler)
+    http.HandleFunc("/edit/", editHandler)
+    // http.HandleFunc("/save/", saveHandler)
     http.ListenAndServe(":8080", nil)
 }
 
